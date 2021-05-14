3d0b386d642a1c14f11f70e218197d50fc2f57b7
Patrik Mrnka <patrikmrnka@Patrik-MacBook-Pro.local>
2021-05-14 16:48:51 +0200

    database connection

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
+<?php
+    if(isset($_POST["submit"])) {
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+        // ověření, zda $username a $password existují = odeslala data z formuláře
+        if ($username && $password) {
+            echo $username;
+            echo "<br>";
+            echo $password;
+            echo "<br>";
+        } else {
+            echo "Něco nám chybí";
+        }
+
+        // připojení do databáze
+        $connection = mysqli_connect("localhost", "root", "", "login_application");
+        
+        if ($connection) {
+            echo "Jsme propojeni s databází";
+        } else {
+            echo "Oooh, něco se pokazilo";
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
