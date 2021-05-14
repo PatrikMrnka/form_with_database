fa0963c562413eca9d93fd2cc916c55d811aafda
Patrik Mrnka <patrikmrnka@Patrik-MacBook-Pro.local>
2021-05-14 16:32:31 +0200

    form HTML

--- index.php
+++ index.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Formulář PHP s databází</title>
 </head>
 <body>
-    
+    <form action="index.php" method="post">
+        <input type="text" name="username" placeholder="Uživatelské jméno">
+        <br>
+        <input type="password" name="password" placeholder="Heslo">
+        <br>
+        <input type="submit" name="submit" value="Odeslat">
+    </form>
 </body>
 </html>
