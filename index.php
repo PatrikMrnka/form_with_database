40894e93a30cd2c0b1a562446978865c224ef076
Patrik Mrnka <patrikmrnka@Patrik-MacBook-Pro.local>
2021-05-14 17:05:36 +0200

    SQL data post

--- index.php
+++ index.php
@@ -14,13 +14,23 @@
         }
 
         // připojení do databáze
-        $connection = mysqli_connect("localhost", "root", "", "login_application");
+        $connection = mysqli_connect("localhost", "root", "", "login_application"); // server, login_name, login_password, database_name
         
-        if ($connection) {
+        if ($connection) { // pokud proměnná $connection něco obsahuje, úspěšně jsme se připojili k databázi
             echo "Jsme propojeni s databází";
         } else {
-            echo "Oooh, něco se pokazilo";
+            // echo "Oooh, něco se pokazilo";
+            die("Oooh, něco se pokazilo");
         }
+
+        $query = "INSERT INTO users(username, password) VALUES('$username','$password')"; // proměnná s příkazem vložení dat z $username a $password do databáze SQL
+
+        $result = mysqli_query($connection, $query); // poslání příkazu z $query do databáze $connection
+
+        if (!$result) { 
+            die("Dotaz do databáze selhal".mysqli_error());
+        }
+
     }
 ?>
 
